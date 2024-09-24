1f06b01d6debf99f2ec1e8e4f2f495f0d04e458c
Milagros Sandoval <milisandoval976@gmail.com>
2024-09-23 22:26:12 -0300

    :fist commit

---
+++ EFWebApi/EFWebApi/Data/Repositories/LibroRepository.cs
@@ -0,0 +1,54 @@
+﻿using EFWebApi.Models;
+
+namespace EFWebApi.Data.Repositories
+{
+    public class LibroRepository : ILibroRepository
+    {
+        private LibrosDbContext _context;
+
+        public LibroRepository(LibrosDbContext context)
+        {
+            _context = context;
+        }
+
+        //public void SetContext(LibrosDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        public void Create(Libro libro)
+        {
+            _context.Libros.Add(libro);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var libroDelete = GetById(id);
+            if(libroDelete != null)
+            {
+                _context.Libros.Remove(libroDelete);
+                _context.SaveChanges();
+            }
+        }
+
+        public List<Libro> GetAll()
+        {
+            return _context.Libros.ToList();
+        }
+
+        public Libro? GetById(int id)
+        {
+            return _context.Libros.Find(id);
+        }
+
+        public void Update(Libro libro)
+        {
+            if (libro != null)
+            {
+                _context.Libros.Update(libro);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
